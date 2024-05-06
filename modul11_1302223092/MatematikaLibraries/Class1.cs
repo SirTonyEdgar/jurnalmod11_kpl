c3d5a5bd0e257626acc41f9ced7bf313eb8ad0b2
SirTonyEdgar <121780770+SirTonyEdgar@users.noreply.github.com>
2024-05-06 20:06:20 +0700

    Add files via upload

---
+++ modul11_1302223092/MatematikaLibraries/Class1.cs
@@ -0,0 +1,47 @@
+﻿namespace UtilityLibraries;
+
+public class MatematikaLibraries
+{
+    public static int FPB(int a, int b)
+    {
+        return b == 0 ? a : FPB(b, a % b);
+    }
+
+    public static int KPK(int a, int b)
+    {
+        return (a / FPB(a, b)) * b;
+    }
+
+    public static String Turunan(int[] persamaan)
+    {
+        String result = "";
+        int first_num;
+        for (int i = 0, pangkat = persamaan.Length - 1; i < persamaan.Length - 2; i++, pangkat--)
+        {
+            first_num = persamaan[i] * pangkat;
+            result +=
+            (first_num == 1 ? null : (first_num).ToString(" + #; -#; 0")) +
+            "x^" + (pangkat - 1 == 1 ? null : (pangkat - 1));
+        }
+        return (result[1].ToString() == "+" ||
+        result[1].ToString() == "-" ? result.Substring(3) : result) +
+        persamaan[^2].ToString(" + #; - #;0");
+    }
+
+    public static String Integral(int[] persamaan)
+    {
+        String result = "";
+        int first_num;
+        for (int i = 0, pangkat = persamaan.Length - i - 1;
+        i < persamaan.Length; i++, pangkat--)
+        {
+            first_num = persamaan[i] / (pangkat + 1);
+            result += (first_num == 1 ? null :
+            first_num.ToString(" + #; -#; 0")) + "x" + (i == persamaan.Length -
+            1 ? null : (pangkat + 1));
+        }
+        return (result[1].ToString() == "+" ||
+        result[1].ToString() == "-") ?
+        result.Substring(3) : result + " + C";
+    }
+}
---
+++ modul11_1302223092/Showcase/Program.cs
@@ -0,0 +1,29 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UtilityLibraries.MatematikaLibraries mat = new UtilityLibraries.MatematikaLibraries();
+
+        int fpb = UtilityLibraries.MatematikaLibraries.FPB(60, 45);
+
+        Console.WriteLine("Hasil FPB: ");
+        Console.WriteLine(fpb);
+
+        int kpk = UtilityLibraries.MatematikaLibraries.FPB(12, 8);
+
+        Console.WriteLine("Hasil KPK: ");
+        Console.WriteLine(kpk);
+
+        string turun = UtilityLibraries.MatematikaLibraries.Turunan([1, 4, -12, 9]);
+
+        Console.WriteLine("Hasil Turunan: ");
+        Console.WriteLine(turun);
+
+        string integral = UtilityLibraries.MatematikaLibraries.Integral([4, 6, -12, 9]);
+
+        Console.WriteLine("Hasil Integral: ");
+        Console.WriteLine(integral);
+    }
+}
